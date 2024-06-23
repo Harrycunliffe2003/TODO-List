195737a1d2735b9cd09432d63fe761e116c31c3d
harry <harrycunliffe2017@outlook.com>
2024-06-23 16:50:54 +0100

    JS Changes

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-$(".button").click(function () { 
-    let item = $(".input").val();
-    if (item == ""){
-        return;         //If no content, do nothing
-    }
-    $(".list").append(`<li class="listItem">${item}</li>`); ///Add content to list
-    $(".input").val("");
+$(document).ready(function () {
+    let count = 0;
+    $(".button").click(function () { 
+
+        let item = $(".input").val();
+        if (item == ""){
+            return;         //If no content, do nothing
+        }
+        $(".list").append(`<li class="listItem" id="listItem${count}">${item}<svg id="${count}" class="trash" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+            </svg></li>`)
+            ///Add content to list
+        $(".input").val("");
+        count = count + 1;
+
+    });
+    $(document).on('click', `.trash`, function (e) {
+        e.preventDefault();
+        let className = e.currentTarget.className.baseVal;
+        let id = e.currentTarget.id;
+        if(className == 'trash'){
+            $(`#listItem${id}`).remove();
+            count = count - 1;
+        }
+        return;
+    });
 });
