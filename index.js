67a889bd2766d92ea5d427ad89f66c25b2bcbd3f
harry <harrycunliffe2017@outlook.com>
2024-06-23 20:02:53 +0100

    local storage

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
     let count = 0;
+    if(localStorage.getItem('count')!= null){
+        count = parseInt(localStorage.getItem('count'))+1;
+    }
+    for(let i = 0;i<localStorage.length;i++){
+        let key = localStorage.key(i);
+        if(key == 'debug' || key == 'count'){
+            continue;
+        }
+        let item = localStorage.getItem(key);
+        $(".list").append(`<li class="listItem" id="listItem${key}">${item}<svg id="${key}" class="trash" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
+            </svg></li>`)
+        
+    }
     $(".button").click(function () { 
 
         let item = $(".input").val();
@@ -11,6 +25,8 @@ $(document).ready(function () {
             </svg></li>`)
             ///Add content to list
         $(".input").val("");
+        localStorage.setItem('count', count);
+        localStorage.setItem(count, item);
         count = count + 1;
 
     });
@@ -20,8 +36,9 @@ $(document).ready(function () {
         let id = e.currentTarget.id;
         if(className == 'trash'){
             $(`#listItem${id}`).remove();
-            count = count - 1;
+            localStorage.removeItem(id);
         }
         return;
     });
+    
 });
